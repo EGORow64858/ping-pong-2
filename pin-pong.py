225188b0dac8c35c8e609f73cb3dba3a301771c7
EGORow64858 <105112853+EGORow64858@users.noreply.github.com>
2022-05-14 16:49:02 +0300

    Add files via upload
    window

---
+++ pin-pong.py
@@ -0,0 +1,20 @@
+from pygame import *
+
+
+window = display.set_mode((700, 500))
+background = transform.scale(
+    image.load('ping-pong table.jpg'),
+    (700,500)
+)
+
+fps = 60
+clock = time.Clock()
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.blit(background, (0,0))
+    display.update()
+    clock.tick(fps)
